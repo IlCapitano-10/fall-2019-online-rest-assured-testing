b1ffca0dadf8110063f99d368563e85a63f065af
Astrit <astrit989@gmail.com>
2020-05-18 18:08:05 -0500

    comm

--- src/main/java/com/automation/tests/day2/ORDSTests.java
+++ src/main/java/com/automation/tests/day2/ORDSTests.java
@@ -1,4 +1,23 @@
 package com.automation.tests.day2;
 
+import io.restassured.response.Response;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
 public class ORDSTests {
+
+
+    String BASE_URL="http://3.90.112.152:1000/ords/hr";
+
+@Test
+@DisplayName("Get list of all employess")
+    public void getAllEmployees(){
+    Response response=given().
+
+            baseUri(BASE_URL).
+            when().
+            get("/employees").prettyPeek();
+
+    }
 }
